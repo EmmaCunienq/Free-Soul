2cdf4177a4cabe7f654cf2c85af42b8386f08de0
EmmaCunienq <3hda07@gmail.com>
2025-03-26 16:28:01 +0100

    Add - methods PortalLight script
    CheckSoulColor method - Compare soul color with the goal color of the light
    ActiveLight method - change color of the light and set isActive var to true

--- Assets/Free Spirit/Scripts/PortalLight.cs
+++ Assets/Free Spirit/Scripts/PortalLight.cs
@@ -2,25 +2,39 @@ using UnityEngine;
 
 public class PortalLight : MonoBehaviour
 {
-    public Color goalColor;
+    private Color goalColor;
+    private bool isActive;
 
     private void Start()
     {
-            if(gameObject.name == "Red light")
+        if(gameObject.name == "Red light")
         {
             goalColor = Color.red;
         }
-            else if (gameObject.name == "Green Light")
+        else if (gameObject.name == "Green Light")
         {
             goalColor = Color.green;
         }
-            else if (gameObject.name == "Blue Light")
+        else if (gameObject.name == "Blue Light")
         {
             goalColor = Color.blue;
         }
-            else
+        else
         {
             Debug.Log("Cette lumière : " + gameObject + " n'est pas bien nommée !");
         }
+
+        isActive = false;
+    }
+
+    public bool CheckSoulColor (GameObject soul)
+    {
+        return goalColor == soul.GetComponent<SpriteRenderer>().color;
+    }
+
+    public void ActiveLight ()
+    {
+        GetComponent<SpriteRenderer>().color = goalColor;
+        isActive = true;
     }
 }
