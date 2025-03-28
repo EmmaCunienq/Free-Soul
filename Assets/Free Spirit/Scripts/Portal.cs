f979fe6f8af2a119ba8ec080c5ff3483a873f921
EmmaCunienq <3hda07@gmail.com>
2025-03-28 22:43:29 +0100

    Add - CheckActivation method Portal script
    Check if all the light are on and call the SkyLight method

--- Assets/Free Spirit/Scripts/Portal.cs
+++ Assets/Free Spirit/Scripts/Portal.cs
@@ -1,17 +1,28 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Timeline;
 
 public class Portal : MonoBehaviour
 {
+    public bool portalActivation;
+
     public bool isPlayerOver;
     private GameObject player;
 
+    private PortalLight[] portalLights;
+
+    private EndLight skyLight;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
+        portalActivation = false;
 
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        portalLights = FindObjectsByType<PortalLight>(FindObjectsSortMode.None);
+
+        skyLight = FindFirstObjectByType<EndLight>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -28,4 +39,15 @@ public class Portal : MonoBehaviour
             isPlayerOver = false;
         }
     }
+
+    public void CheckActivation ()
+    {
+        if (portalLights.All(portal => portal.isActive))
+        {
+            portalActivation = true;
+
+            StartCoroutine(skyLight.SkyLight());
+        }
+    }
+
 }
