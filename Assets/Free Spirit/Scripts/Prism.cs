e2b16c80aa57e6ea57e94903352d5855e95a0b90
EmmaCunienq <3hda07@gmail.com>
2025-03-31 18:36:39 +0200

    Add - ActivatePrism methode Prism script
    ActivatePrism - set isActive to true and set color to the color of the prism

--- Assets/Free Spirit/Scripts/Prism.cs
+++ Assets/Free Spirit/Scripts/Prism.cs
@@ -2,23 +2,39 @@ using UnityEngine;
 
 public class Prism : MonoBehaviour
 {
-    public bool isActive;
+    private bool isActive;
+    private Color prismColor;
 
     private void Start()
     {
         if(gameObject.name == "Water prism")
         {
-            GetComponent<SpriteRenderer>().color = Color.blue;
+            prismColor = Color.blue;
             isActive = true;
         }
         else if (gameObject.name == "Earth prism")
         {
-            GetComponent<SpriteRenderer>().color = Color.green;
+            prismColor = Color.green;
             isActive = true;
         }
         else if (gameObject.name == "Fire prism")
         {
+            prismColor = Color.red;
             isActive = false;
+        }
+
+        if (isActive)
+        {
+            GetComponent<SpriteRenderer>().color = prismColor;
+        }
+    }
+
+    public void ActivatePrism ()
+    {
+        isActive = true;
+        if (isActive)
+        {
+            GetComponent<SpriteRenderer>().color = prismColor;
         }
     }
 }
